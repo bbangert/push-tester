a9515e3d2446cc1f78aa6973d77dc79d4fce28a3
Ben Bangert <ben@groovie.org>
2015-02-10 10:04:26 -0800

    Cleanups and refactor

--- spTester/Main.hs
+++ spTester/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 
 module Main where
 
@@ -36,6 +37,7 @@ main = getArgs >>= parseArguments >>= maybe (return ()) runTester
 defaultSettings :: Map.Map String Int
 defaultSettings = Map.fromList [("NOTIFICATION_DELAY", 5),
                                 ("NOTIFICATION_COUNT", 20),
+                                ("NOTIFICATION_MAX_SIZE", 4096),
                                 ("PING_DELAY", 20),
                                 ("PING_COUNT", 10),
                                 ("RECONNECT_DELAY", 5)]
@@ -56,7 +58,7 @@ parseArguments [ip, port, spawnCount, strategy, statsdHost] = do
     clientTracker <- newClientTracker maxC
     envSettings <- zip vars <$> mapM getEnv vars
 
-    let actualSettings = Map.fromList $ 
+    let actualSettings = Map.fromList $
             map (second $ read . fromJust) $ filter (isNothing . snd) envSettings
         settingsMap = Map.union actualSettings defaultSettings
         tconfig = TC ip (read port) clientTracker newStorage sink sess settingsMap
@@ -101,6 +103,11 @@ decRef ref = void $ atomicModifyIORef' ref (\x -> (x-1, ()))
 
 -}
 
+type Now = Int
+type Delay = Int
+type LastUsed = Int
+type PossibleInteraction = Now -> LastUsed -> WebsocketInteraction LastUsed
+
 emptyNotification :: Notification
 emptyNotification = Notification Nothing Nothing
 
@@ -125,14 +132,28 @@ setupNewEndpoint = do
 -- | Helper function that determines if the supplied action should be run based
 --   on whether or not enough of a delay has passed
 --   Returns the time to use as the new 'lastUsed' value.
-afterDelay :: Int                       -- Current 'time' in seconds
-           -> Int                       -- Last 'time' the action ran.
-           -> Int                       -- Delay after which action runs
-           -> WebsocketInteraction a    -- Action to run, result is dropped.
-           -> WebsocketInteraction Int  -- Returns new 'lastUsed' value
-afterDelay now lastUse delay action
+afterDelay :: WebsocketInteraction a -> Delay -> PossibleInteraction
+afterDelay action now lastUse delay
     | now-lastUse > delay = action >> return now
     | otherwise           = return lastUse
+
+-- | Helper to create a push notification PossibleInteraction
+pushAction :: (ChannelID, Endpoint) -> Delay -> PossibleInteraction
+pushAction endpoint delay = afterDelay push delay
+  where
+    push = sendPushNotification endpoint emptyNotification >>= ack
+
+-- | Loop helper that will iterate forever, running the list of interactions and
+--   then waiting 1 second.
+runLoop :: [PossibleInteraction] -> WebsocketInteraction ()
+runLoop actions = go 0 $ zip actions (repeat 0)
+  where
+    go :: Now -> [(PossibleInteraction, LastUsed)] -> WebsocketInteraction ()
+    go now actionList = do
+        actionList' <- mapM (runIt now) actionList
+        wait 1
+        go (now+1) actionList'
+    runIt now (action, used) = (action,) <$> action now used
 
 -- | Basic single channel registration that sends a notification to itself
 --  every 5 seconds and never pings
@@ -142,10 +163,7 @@ basic = do
     withConnection $ do
         void $ helo Nothing (Just [])
         endpoint <- setupNewEndpoint
-        forever $ do
-            msg <- sendPushNotification endpoint emptyNotification
-            ack msg
-            wait notifDelay
+        runLoop [pushAction endpoint notifDelay]
 
 -- | Delivers a notification once every 10 seconds, pings every 20 seconds
 pingDeliver :: TestInteraction ()
@@ -155,18 +173,7 @@ pingDeliver = do
     withConnection $ do
         void $ helo Nothing (Just [])
         endpoint <- setupNewEndpoint
-        loop 0 endpoint 0 0 notifDelay pingDelay
-  where
-    loop :: Int -> (ChannelID, Endpoint)
-         -> Int -> Int
-         -> Int -> Int
-         -> WebsocketInteraction ()
-    loop count endpoint lastPing lastNotif notifDelay pingDelay = do
-        lastPing' <- afterDelay count lastPing pingDelay ping
-        lastNotif' <- afterDelay count lastNotif notifDelay $
-            sendPushNotification endpoint emptyNotification >>= ack
-        wait 1
-        loop (count+1) endpoint lastPing' lastNotif' notifDelay pingDelay
+        runLoop [afterDelay ping pingDelay, pushAction endpoint notifDelay]
 
 -- | Registers a new channel ID every 10 seconds
 --   Chooses a channel randomly every 5 seconds for delivery and sends a push
@@ -229,23 +236,21 @@ reconnecter = do
 -- | Registers a channel, sends a notification with data every 5 seconds,
 --   pings every 20
 dataSender :: TestInteraction ()
-dataSender = withConnection $ do
-    void $ helo Nothing (Just [])
-    endpoint <- setupNewEndpoint
-    loop 0 endpoint
+dataSender = do
+    [notifDelay, notifMaxSize, pingDelay] <-
+        mapM getSetting ["NOTIFICATION_DELAY", "NOTIFICATION_MAX_SIZE",
+                         "PING_DELAY"]
+    withConnection $ do
+        void $ helo Nothing (Just [])
+        endpoint <- setupNewEndpoint
+        let pingAction = afterDelay ping pingDelay
+            pushAction = afterDelay (push notifMaxSize endpoint) notifDelay
+        runLoop [pingAction, pushAction]
   where
-    loop :: Int -> (ChannelID, Endpoint) -> WebsocketInteraction ()
-    loop count endpoint = do
-        len <- randomNumber (10, 4000)
+    push maxSize endpoint = do
+        len <- randomNumber (10, maxSize)
         dat <- randomData len
         msg <- sendPushNotification endpoint (Notification Nothing (Just dat))
         ack msg
         let msgData = fromJust . cu_data . head . fromJust $ updates msg
         assert (dat==msgData, msg) "Data failed to match"
-        if count == 20 then do
-            void ping
-            wait 5
-            loop 5 endpoint
-        else do
-            wait 5
-            loop (count+5) endpoint
